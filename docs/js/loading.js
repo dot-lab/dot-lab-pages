cf0d73c09f8d16c367ea96e73d0ab69b0e854740
yoshie <info@dot-lab.xyz>
2019-02-02 14:17:58 +0900

    ロード画面の実装

---
+++ docs/js/loading.js
@@ -0,0 +1,10 @@
+$(function() {
+  var h = $(window).height();
+  $('#wrap').css('display','none');
+  $('#loader-bg ,#loader').height(h).css('display','block');
+});
+function hideLoading() {
+  $('#loader-bg').delay(900).fadeOut(800);
+  $('#loader').delay(600).fadeOut(300);
+  $('#wrap').css('display','block');
+}
--- docs/js/main.js
+++ docs/js/main.js
@@ -1,110 +1,73 @@
 const config = {
-    apiKey: "AIzaSyCWFb4g7iiYndrM1CPM2_VezjuTBPKlUaA",
-    authDomain: "sekai-8fcfd.firebaseapp.com",
-    databaseURL: "https://sekai-8fcfd.firebaseio.com",
-    projectId: "sekai-8fcfd",
-    storageBucket: "sekai-8fcfd.appspot.com",
-    messagingSenderId: "713119701258"
-  };
-  const firebaseApp = firebase.initializeApp(config);
-  const app = flamelink({ firebaseApp });
+  apiKey: "AIzaSyCWFb4g7iiYndrM1CPM2_VezjuTBPKlUaA",
+  authDomain: "sekai-8fcfd.firebaseapp.com",
+  databaseURL: "https://sekai-8fcfd.firebaseio.com",
+  projectId: "sekai-8fcfd",
+  storageBucket: "sekai-8fcfd.appspot.com",
+  messagingSenderId: "713119701258"
+};
+const firebaseApp = firebase.initializeApp(config);
+const app = flamelink({ firebaseApp });
 
-  function getPostsData() {
-    return app.content.get('myPosts');
-  }
-  function setPostsDataToArray(promise) {
-    let postsArr = [];
-    promise.then( posts =>{
-      for(let i in posts) {
-        let content = {
-          year: posts[i].date.slice(0,4),
-          month: posts[i].date.slice(5,7),
-          date: posts[i].date.slice(0,4)+'.'+posts[i].date.slice(5,7),
-          title: posts[i].title,
-          editor:  posts[i].editor,
-          imgId: posts[i].image[0].toString(),
-          tech: posts[i].tech
-        }
-        postsArr.push(content);
-      }
-    }).then(function(){
-      postsArr.sort(function(a,b){
-        return b.year - a.year;
-      });
-    });
-    return postsArr;
-  }
-  async function getImgUrlArr(promiseArr) {
-    let result = [];
-    for(let i = 0;i<promiseArr.length;i++) {
-      result[i] = await promiseArr[i]
-    }
-    return result;
-  }
-
+async function getPostsData() {
   let postsData = [];
-  new Promise(function(resolve){
-    setTimeout(function(){
-      resolve(0);
-    },500);
-  })
-  .then(function(postsData){
-    return new Promise(function(resolve) {
-      setTimeout(function(){
-        postsData = setPostsDataToArray(getPostsData());
-        resolve(postsData);
-      },200)
-    });
-  })
-  .then(function(postsData){
-    return new Promise(function(resolve){
-      setTimeout(function(){
-        for(let i in postsData) {
-          postsData[i].imgUrl = app.storage.getURL(postsData[i].imgId);
-        }
-        resolve(postsData);
-      },200);
-    });
-  })
-  .then(function(postsData){
-    let values = [];
-    return new Promise(function(resolve) {
-      setTimeout(function(){
-        for(let i in postsData) {
-          values.push(app.storage.getURL(postsData[i].imgId));
-        }
-        resolve([postsData,values]);
-      },200);
-    });
-  })
-  .then(function(result){
-    let promiseArr = getImgUrlArr(result[1]);
-    let value;
-    return new Promise(resolve=>{
-      setTimeout(()=>{
-        value = 
-          promiseArr.then(value=>{
-            for(let i = 0;i<result[1].length;i++) {
-              result[0][i].imgUrl = value[i];
-            }
-            return result[0];
-          })
-        resolve(value)
-      },200)
-    })
-  })
-  .then(postsData=>{
-    let postsArea = document.getElementById('posts');
-    for(let i = 0;i<postsData.length;i++){
-      let target = document.createElement('div');
-      target.classList.add('post');
-      target.innerHTML =
-      '<h1>'+ postsData[i].title +'</h1>'+
-      '<h2>'+ postsData[i].date +'</h2>'+
-      '<p>'+ postsData[i].tech +'</p>'+
-      '<a data-lightbox="works" data-title="'+ postsData[i].title +'" href="'+ postsData[i].imgUrl +'"><img src="'+ postsData[i].imgUrl +'"></a>' +
-      '<div class="content">'+ postsData[i].editor +'</div>'+
-      '<hr>';
-      postsArea.appendChild(target);
+  let promise = app.content.get('myPosts');
+  postsData.push(await promise);
+  return postsData[0];
+}
+async function setPostsDataToArray(posts) {
+  let postsArr = [];
+  let datas = await posts;
+  for(key in datas){
+    let content = {
+      year: datas[key].date.slice(0,4),
+      month: datas[key].date.slice(5,7),
+      date: datas[key].date.slice(0,4)+'.'+datas[key].date.slice(5,7),
+      title: datas[key].title,
+      editor:  datas[key].editor,
+      imgId: datas[key].image[0].toString(),
+      tech: datas[key].tech
     }
+    postsArr.push(content);
+  }
+  postsArr.sort(function(a,b){
+    return b.year - a.year;
   });
+  return postsArr;
+}
+
+async function setImgUrlToArr(postsArr) {
+  let arr = await postsArr;
+  for(key in arr) {
+    arr[key].imgUrl = await app.storage.getURL(arr[key].imgId);
+  }
+  return arr;
+}
+
+async function show(){
+  let postsArr = await setPostsDataToArray(getPostsData());
+  postsArr = await setImgUrlToArr(postsArr);
+  let elemArr = [];
+  for(let i = 0;i<postsArr.length;i++){
+    let target = document.createElement('div');
+    target.classList.add('post');
+    target.innerHTML =
+    '<h1>'+ postsArr[i].title +'</h1>'+
+    '<h2>'+ postsArr[i].date +'</h2>'+
+    '<p>'+ postsArr[i].tech +'</p>'+
+    '<a data-lightbox="works" data-title="'+ postsArr[i].title +'" href="'+ postsArr[i].imgUrl +'"><img src="'+ postsArr[i].imgUrl +'"></a>' +
+    '<div class="content">'+ postsArr[i].editor +'</div>'+
+    '<hr>';
+    elemArr.push(target);
+  }
+  return new Promise(function(resolve) {
+    resolve(elemArr);
+  })
+}
+show().then(function(data){
+  let postsArea = document.getElementById('posts');
+  for(i in data) {
+    postsArea.appendChild(data[i]);
+  }
+  hideLoading();
+})
